1d515195837461dbbf09e6bf6ba329b6dd42e74e
Anand Chandak <chandakme@gmail.com>
2017-03-28 12:50:54 +0530

    Update runEmailApp.sh
    passed the jacksoncore asl jar in the classpath

--- EmailApp/scripts/runEmailApp.sh
+++ EmailApp/scripts/runEmailApp.sh
@@ -1,3 +1,2 @@
 #Once the tables are defined run email client application
-java -cp ../bin:../lib/kvclient.jar:$KVHOME/lib/jackson-mapper-asl.jar com.oracle.email.console.EmailCLI  localhost:5000 kvstore
- 
+java -cp ../bin:../lib/kvclient.jar:$KVHOME/lib/jackson-mapper-asl.jar:$KVHOME/lib/jackson-core-asl.jar com.oracle.email.console.EmailCLI  localhost:5000 kvstore
