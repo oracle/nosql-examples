83c50d385d2cab3ab8e8e33cf22b28c4f7053b51
Anand Chandak <chandakme@gmail.com>
2017-03-28 12:53:13 +0530

    Update startKVLite.sh
    Added the security config option to startkvlite

--- EmailApp/scripts/startKVLite.sh
+++ EmailApp/scripts/startKVLite.sh
@@ -3,4 +3,4 @@
 
 #Start KVLite from the kvroot
 echo "Starting KVLite ..."
-nohup java -jar $KVHOME/lib/kvstore.jar kvlite -root $KVROOT -host localhost 2>&1 > /tmp/kvlite.out &
+nohup java -jar $KVHOME/lib/kvstore.jar kvlite -root $KVROOT -host localhost -secure-config disable 2>&1 > /tmp/kvlite.out &
