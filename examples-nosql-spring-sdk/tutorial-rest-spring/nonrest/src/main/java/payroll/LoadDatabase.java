988e435ebd4b0e03ea783c7dc1a52ad5fdf766f8
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2022-11-09 13:42:05 +0100

    with JPA/H2

---
+++ examples-nosql-spring-sdk/tutorial-rest-spring/nonrest/src/main/java/payroll/LoadDatabase.java
@@ -0,0 +1,22 @@
+package payroll;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class LoadDatabase {
+
+  private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+  @Bean
+  CommandLineRunner initDatabase(EmployeeRepository repository) {
+
+    return args -> {
+      log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
+      log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
+    };
+  }
+}
---
+++ examples-nosql-spring-sdk/tutorial-rest-spring/nonrest/src/test/java/payroll/PayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
