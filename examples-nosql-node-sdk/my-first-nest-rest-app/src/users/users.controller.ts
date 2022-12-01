3cb5c0ebb0e5c7ec0847d4127a84e46e0372e09f
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2022-12-01 14:24:42 +0100

    nestjs/with-nosql
    -    adding the nosql.service.ts (class NoSQLService)
    -    modifying the main.ts - connection to Oracle NoSQL database
    -    modifying the users.service.ts to call Oracle NoSQL database

--- examples-nosql-node-sdk/my-first-nest-rest-app/src/users/users.controller.ts
+++ examples-nosql-node-sdk/my-first-nest-rest-app/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -13,22 +13,22 @@ export class UsersController {
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  findAll(@Query() params) {
+    return this.usersService.findAll(params);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+    return this.usersService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+    return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+    return this.usersService.remove(id);
   }
 }
