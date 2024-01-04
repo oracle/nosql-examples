d5c357e776e53ec0d6de1832d41e35a42aac3fcd
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2024-01-04 07:59:16 +0100

    QA + new version 23.3

--- examples-nosql-cluster-deployment/script/env.sh
+++ examples-nosql-cluster-deployment/script/env.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2023 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
-export KVHOME=$HOME/nosql/kv-23.1.25
+export KVHOME=$HOME/nosql/kv-23.3.30
 
 export KVROOT=$HOME/nosql/kvroot
 export KVDATA=$HOME/nosql/data
