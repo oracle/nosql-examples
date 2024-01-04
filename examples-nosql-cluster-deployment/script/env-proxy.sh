a963a23984650fe94bba307142dabac03c1404b4
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2024-01-04 08:14:31 +0100

    Updating Copyright
    Updating Copyright only for files changed during this QA/New Release

--- examples-nosql-cluster-deployment/script/env-proxy.sh
+++ examples-nosql-cluster-deployment/script/env-proxy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2023 Oracle and/or its affiliates.
+# Copyright (c) 2023, 2024 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
 export KVHOME=$HOME/nosql/kv-23.3.30
