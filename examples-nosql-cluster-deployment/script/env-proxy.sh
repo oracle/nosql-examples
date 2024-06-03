da392d96f3f237c61432c3a237020e2fd356498a
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2024-06-03 15:44:44 +0200

    examples-nosql-cluster-deployment: QA + new version 24.1 (#172)
    * QA new version 24.1.15
    
    * Update examples-nosql-cluster-deployment.zip

--- examples-nosql-cluster-deployment/script/env-proxy.sh
+++ examples-nosql-cluster-deployment/script/env-proxy.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2023, 2024 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
-export KVHOME=$HOME/nosql/kv-23.3.32
+export KVHOME=$HOME/nosql/kv-24.1.15
 export KVHOST=`hostname`
 export KVSTORE=OUG
 export PROXYHOME=/home/opc/proxy
--- examples-nosql-cluster-deployment/script/env.sh
+++ examples-nosql-cluster-deployment/script/env.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2023, 2024 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
-export KVHOME=$HOME/nosql/kv-23.3.32
+export KVHOME=$HOME/nosql/kv-24.1.15
 
 export KVROOT=$HOME/nosql/kvroot
 export KVDATA=$HOME/nosql/data
