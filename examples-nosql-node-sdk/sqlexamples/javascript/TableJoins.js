961d8b5cda4247b1dbfa1ad72ec27ff66bbe52d6
Purnima <purnima.subramanian@oracle.com>
2025-03-25 11:48:50 +0530

    Update TableJoins.js

--- examples-nosql-node-sdk/sqlexamples/javascript/TableJoins.js
+++ examples-nosql-node-sdk/sqlexamples/javascript/TableJoins.js
@@ -82,6 +82,7 @@ async function dotablejoins() {
       }`
       const stmt_loj = 'SELECT * FROM ticket a LEFT OUTER JOIN ticket.bagInfo.flightLegs b ON a.ticketNo=b.ticketNo';
       const stmt_nt = 'SELECT * FROM NESTED TABLES (ticket a descendants(ticket.bagInfo.flightLegs b))';
+      const stmt_ij = 'SELECT * FROM ticket a, ticket.bagInfo.flightLegs b WHERE a.ticketNo=b.ticketNo';	  
       await createTable(handle,regtbl_DDL,'true',table_name);
       await createTable(handle,childtbl_DDL,'false',childtable_name);
       await createTable(handle,desctbl_DDL,'false',desctable_name);
@@ -95,6 +96,8 @@ async function dotablejoins() {
       await fetchData(handle,stmt_loj);
       console.log("Fetching data using NESTED TABLES");
       await fetchData(handle,stmt_nt);
+      console.log("Fetching data using Inner Joins");
+      await fetchData(handle,stmt_ij);		  
    } catch (error ) {
       console.log(error);
    }
